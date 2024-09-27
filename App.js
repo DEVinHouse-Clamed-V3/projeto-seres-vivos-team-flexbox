42fd7d426044b90bfdae314d2cf33669cd744b3b
Lucas da Silva <sklucassilva@gmail.com>
2024-09-26 23:06:41 -0300

    Refactor Protista page to remove header title

--- App.js
+++ App.js
@@ -8,18 +8,25 @@ import Protista from './src/pages/Protista';
 import Monera from './src/pages/Monera';
 import Home from './src/pages/Home';
 
-
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} options={{title:'Reino Animal'}}/>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: 'Reino Animal' }}
+        />
         <Stack.Screen name="Animal" component={Animal} />
         <Stack.Screen name="Plantae" component={Plantas} />
         <Stack.Screen name="Fungi" component={Fungos} />
-        <Stack.Screen name="Protista" component={Protista} />
+        <Stack.Screen
+          name="Protista"
+          component={Protista}
+          options={{ headerTitle: '' }}
+        />
         <Stack.Screen name="Monera" component={Monera} />
       </Stack.Navigator>
     </NavigationContainer>
