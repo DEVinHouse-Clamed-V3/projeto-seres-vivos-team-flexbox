5bdf784d41c65ef8caa5774921384b89c862aa71
lppellin <thelucaspellin@gmail.com>
2024-09-28 15:59:58 -0300

    add card shadow

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -72,6 +72,14 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 10,
     marginBottom: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
 
   title: {
--- src/pages/Plantas.js
+++ src/pages/Plantas.js
@@ -1,15 +1,88 @@
-import React from 'react';
-import { Text, SafeAreaView } from 'react-native';
-import { globalStyles } from '../styles/global';
+import { Text, SafeAreaView, StyleSheet, Alert, FlatList, TextInput, Image } from 'react-native';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import PlantCard from '../components/PlantCard';
 
-const Plantas  = () => {
+const Plantas = () => {
+
+  const [plantas, setPlantas] = useState([]);
+  const [search, setSearch] = useState('');
+
+  // const filtroPlantas = plantas.filter(biscoito => biscoito.mensagem.toUpperCase().includes(search.toUpperCase()))
+
+
+  useEffect(() => {
+    axios
+      .get('http://192.168.1.14:3000/plantas')
+      .then((response) => {
+        setPlantas(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+
 
   return (
-    <SafeAreaView style={globalStyles.container}>
-      <Text>Tela do reino plantas</Text>
+    <SafeAreaView style={styles.container}>
+
+      <TextInput
+        style={styles.searchInput}
+        value={search}
+        onChangeText={setSearch}
+        placeholder="Pesquisar"
+      />
+
+      <FlatList
+        data={plantas}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <PlantCard item={item} />}
+
+        ListEmptyComponent={
+          <Text style={styles.empty}>Nenhum dado encontrado.</Text>
+        }
+
+        contentContainerStyle={{
+          justifyContent: 'center',
+          gap: 20,
+          paddingBottom: 15,
+        }}
+
+
+      />
+
+
     </SafeAreaView>
   );
 };
+
+
+const styles = StyleSheet.create({
+
+  container:{
+    flex: 1,
+    backgroundColor: '#2b5a4261',
+  },
+
+
+  searchInput: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,    
+  },
+
+  empty: {
+    textAlign: 'center',
+    marginTop: 20
+  },
+
+
+
+
+})
+
 
 
 export default Plantas;
