9f26af342539227712d69eabf081eca176138a9d
lppellin <thelucaspellin@gmail.com>
2024-09-28 15:54:33 -0300

    componente card

---
+++ src/components/PlantCard.js
@@ -0,0 +1,91 @@
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+export default function PlantCard({ item }) {
+
+  return (
+
+    <View style={styles.cardContainer}>
+      <Image source={{ uri: item.image }} style={styles.img} />
+
+      <View style={styles.cardRow}>
+        <Text style={styles.title}>Nome:</Text>
+        <Text style={styles.plantTitle}>{item.name}</Text>
+      </View>
+
+      <View style={styles.cardRow}>
+        <Text style={styles.title}>Descrição:</Text>
+        <Text style={styles.description} >{item.description}</Text>
+      </View>
+
+      <View style={styles.cardRow}>
+        <Text style={styles.title}>Nutrição:</Text>
+        <Text>{item.nutrition}</Text>
+      </View>
+
+      <View style={styles.cardRow}>
+        <Text style={styles.title}>Tipo celular:</Text>
+        <Text>{item.cellType}</Text>
+      </View>
+
+      <View style={styles.cardRow}>
+        <Text style={styles.title}>Organização:</Text>
+        <Text>{item.cellOrganization}</Text>
+      </View>
+
+      <View style={styles.cardRow}>
+        <Text style={styles.title}>Reprodução:</Text>
+        <Text>{item.reproduction}</Text>
+      </View>
+
+      <View style={styles.cardRow}>
+        <Text style={styles.title}>Respiração:</Text>
+        <Text>{item.respiration}</Text>
+      </View>
+
+    </View>
+
+  )
+
+}
+
+
+
+const styles = StyleSheet.create({
+  img: {
+    width: 150,
+    height: 150,
+    margin: 10,
+    alignSelf: 'center',
+    borderRadius: 20
+  },
+
+  cardRow: {
+    flexDirection: 'row',
+    margin: 10,
+    gap: 5,
+  },
+
+  cardContainer: {
+    width: '90%',
+    alignSelf: 'center',
+    backgroundColor: '#ffffff',
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+
+  title: {
+    fontWeight: 'bold',
+    color: '#2b5a42',
+  },
+
+  description: {
+    maxWidth: 250,
+  },
+
+  plantTitle: {
+    fontWeight: 'bold',
+  }
+
+
+})
