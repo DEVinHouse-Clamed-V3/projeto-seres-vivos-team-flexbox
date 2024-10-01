b0595447cc7de7c7f3b7f518ab0637f1ff11d7fd
Gusstavosf <gusstavoffc@gmail.com>
2024-09-30 22:02:58 -0300

    Enviando arquios reino animal

---
+++ src/components/ReinoAnimal.jsx
@@ -0,0 +1,94 @@
+import { View, Text, Image, StyleSheet } from 'react-native';
+import React from 'react';
+
+export default function ReinoAnimal({ item }) {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: item.image }} style={styles.image} />
+      <Text style={styles.title}>{item.name}</Text>
+
+      <View style={styles.contentSection}>
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Descrição:</Text>
+          <Text style={styles.text}>{item.description}</Text>
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Nutrição:</Text>
+          <Text style={styles.text}>{item.nutrition}</Text>
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Tipo Celular:</Text>
+          <Text style={styles.text}>{item.cellType}</Text>
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Organização:</Text>
+          <Text style={styles.text}>{item.cellOrganization}</Text>
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Reprodução:</Text>
+          <Text style={styles.text}>{item.reproduction}</Text>
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Respiração:</Text>
+          <Text style={styles.text}>{item.respiration}</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    backgroundColor: '#ffffff',
+    borderRadius: 12,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+  },
+  image: {
+    overflow: 'hidden',
+    resizeMode: 'cover',
+    width: 150,
+    height: 150,
+    borderRadius: 12,
+    marginBottom: 15,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#ea2e0d',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  contentSection: {
+    gap: 5,
+    flexDirection: 'column',
+    width: '100%',
+    alignItems: 'flex-start',
+  },
+  section: {
+    flexDirection: 'column', // Alterado para coluna
+    width: '100%',
+    alignItems: 'flex-start', // Alinhado à esquerda para o texto
+  },
+  sectionTitle: {
+    textAlign: 'left', // Alterado para alinhar à esquerda
+    fontSize: 14,
+    fontWeight: '600',
+    color: '#FF6347',
+  },
+  text: {
+    textAlign: 'left', // Alterado para alinhar à esquerda
+    fontSize: 12,
+    color: '#444444',
+  },
+});
--- src/pages/Animal.js
+++ src/pages/Animal.js
@@ -1,15 +1,61 @@
-import React from 'react';
-import { Text, SafeAreaView } from 'react-native';
-import { globalStyles } from '../styles/global';
+import React, { useEffect, useState } from 'react';
+import { Text, SafeAreaView, StyleSheet, View, FlatList } from 'react-native';
+import axios from 'axios';
+import ReinoAnimal from '../components/ReinoAnimal'
 
-const Animal  = () => {
+export default function Animal(){
 
-  return (
-    <SafeAreaView style={globalStyles.container}>
-      <Text>Tela do reino animal</Text>
-    </SafeAreaView>
-  );
-};
+const [reino, setReino] = useState([]);
 
+useEffect(() => {
+  axios.get('http://192.168.15.95:3001/animais')
+    .then((response) => {
+      setReino(response.data)
+    })
+    .catch(() => {
+      Alert.alert("Não foi possível obter uma lista de animais")
+    })
+}, [])
 
-export default Animal;
+return (
+  <SafeAreaView style={styles.container}>
+    <View style={styles.reino}>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={reino}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <ReinoAnimal item={item} />}
+        ListEmptyComponent={
+          <Text style={styles.empty}>Nenhum dado encontrado.</Text>
+        }
+        style={{ width: '100%' }}
+        contentContainerStyle={{
+          justifyContent: 'center',
+          gap: 20,
+          paddingBottom: 15,
+        }}
+      />
+    </View>
+  </SafeAreaView>
+);
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FF6347',
+    alignItems: 'center',
+    paddingBottom: 30,
+  },
+  reino: {
+    width: '100%',
+    paddingHorizontal: 15,
+    marginTop: 20,
+  },
+  empty: {
+    textAlign: 'center',
+    fontSize: 16,
+    color: '#00796b',
+    marginTop: 20,
+  },
+});
